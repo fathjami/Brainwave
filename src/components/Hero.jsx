942949287c2de7d69d8d1c392a42de98ea8b06fc
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2024-05-16 13:14:17 +0100

    fix section component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
 
     return (
         <Section
-            className="pt-[12rem] -mt-[5.25rem]"
+            className="pt-[17rem] -mt-[5.25rem] "
             crosses
             crossesOffset="lg:translate-y-[5.25rem]"
             customPaddings
