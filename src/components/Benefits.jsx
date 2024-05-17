2c78e0327680e17c9ef36b3076621d0aa2594b85
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2024-05-17 17:17:15 +0100

    fix ClipPath;

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -3,28 +3,28 @@ import Heading from './Heading'
 import Section from './Section'
 import Arrow from '../assets/svg/Arrow'
 import { GradientLight } from './design/Benefits'
-import clipPath from '../assets/svg/ClipPath'
+import ClipPath from '../assets/svg/ClipPath'
 
-function Benefits() {
+const Benefits = () => {
     return (
         <Section id="features">
-            <div className="container relative z-2 ">
+            <div className="container relative z-2">
                 <Heading
-                    className="md:max-w-md lg:max-w-xl"
-                    title="Chat Smarter, Not Harder With Brainwave"
+                    className="md:max-w-md lg:max-w-2xl"
+                    title="Chat Smarter, Not Harder with Brainwave"
                 />
+
                 <div className="flex flex-wrap gap-10 mb-10">
                     {benefits.map((item) => (
                         <div
-                            key={item.id}
-                            className="block relative p-0.5 bg-no-repeat bg-[length_100%_100%]
-                            md:max-w-[24rem]"
+                            className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
                             style={{
                                 backgroundImage: `url(${item.backgroundUrl})`,
                             }}
+                            key={item.id}
                         >
-                            <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none ">
-                                <h5 className="h5 mb-5 ">{item.title}</h5>
+                            <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                                <h5 className="h5 mb-5">{item.title}</h5>
                                 <p className="body-2 mb-6 text-n-3">
                                     {item.text}
                                 </p>
@@ -41,10 +41,12 @@ function Benefits() {
                                     <Arrow />
                                 </div>
                             </div>
+
                             {item.light && <GradientLight />}
+
                             <div
                                 className="absolute inset-0.5 bg-n-8"
-                                style={{ clickPath: 'url(#benefits}' }}
+                                style={{ clipPath: 'url(#benefits)' }}
                             >
                                 <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
                                     {item.imageUrl && (
@@ -58,7 +60,8 @@ function Benefits() {
                                     )}
                                 </div>
                             </div>
-                            <clipPath />
+
+                            <ClipPath />
                         </div>
                     ))}
                 </div>
