c67f2f63c112b90c1d09403f7bac5a884f53e65b
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2024-05-17 18:47:22 +0100

    add collaboration icons"

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,5 +1,5 @@
-import { check } from '../assets'
-import { collabContent } from '../constants'
+import { brainwaveSymbol, check } from '../assets'
+import { collabApps, collabContent, collabText } from '../constants'
 import Button from './Button'
 import Section from './Section'
 
@@ -34,6 +34,42 @@ function Collaboration() {
                         ))}
                     </ul>
                     <Button>Try it now</Button>
+                </div>
+                <div className="lg:ml-auto xl:w-[38rem] mt-4">
+                    <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-22 lg:w-[22rem] lg:mx-auto">
+                        {collabText}
+                    </p>
+                    <div
+                        className="relative left-1/2 flex w-[22rem] aspect-square 
+                    border border-n-6 rounded-full -translate-x-1/2 scale-75 md:scale-100"
+                    >
+                        <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+                            <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+                                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                                    <img
+                                        src={brainwaveSymbol}
+                                        width={48}
+                                        height={48}
+                                        alt="brainwave"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <ul>
+                            {collabApps.map((app, index) => (
+                                <li
+                                    key={app.id}
+                                    className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-[${
+                                        index * 45
+                                    }]`}
+                                >
+                                    <div>
+                                        <img src={app.icon} />
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </Section>
