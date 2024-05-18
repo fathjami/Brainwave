dc25a7c3b249d2b1498894d5749acdeeaf17c7e3
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2024-05-18 19:11:27 +0100

    complete services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
 import Section from './Section'
 import Heading from './Heading'
-import { check, service1, service2 } from '../assets'
-import { brainwaveServices } from '../constants'
-import Generating from './Generating'
+import { check, service1, service2, service3 } from '../assets'
+import { brainwaveServices, brainwaveServicesIcons } from '../constants'
 import {
-    PhotoChatMessage,
     Gradient,
+    PhotoChatMessage,
     VideoBar,
     VideoChatMessage,
 } from './design/Services'
+import Generating from './Generating'
 
 function Services() {
     return (
@@ -84,7 +83,58 @@ function Services() {
                             </div>
                             <PhotoChatMessage />
                         </div>
+                        <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+                            <div className="py-12 px-4 xl:px-8">
+                                <h4 className="h4 mb-4">Video generation</h4>
+                                <p className="body-2 mb-[2rem] text-n-3">
+                                    The world’s most powerful AI photo and video
+                                    art generation engine. What will you create?
+                                </p>
+                                <ul className="flex items-center justify-between">
+                                    {brainwaveServicesIcons.map(
+                                        (item, index) => (
+                                            <li
+                                                key={index}
+                                                className={`rounded-2xl flex items-center justify-center
+                                                ${
+                                                    index === 2
+                                                        ? 'w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[3.5rem] md:h-[3.5rem]'
+                                                        : 'flex w-10 h-10 bg-n-6 md:w-15 md:h-15'
+                                                }`}
+                                            >
+                                                <div
+                                                    className={
+                                                        index === 2
+                                                            ? 'flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]'
+                                                            : ''
+                                                    }
+                                                >
+                                                    <img
+                                                        src={item}
+                                                        width={24}
+                                                        height={24}
+                                                        alt={item}
+                                                    />
+                                                </div>
+                                            </li>
+                                        )
+                                    )}
+                                </ul>
+                            </div>
+                            <div className="relative  h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
+                                <img
+                                    src={service3}
+                                    className="w-full h-full object-cover"
+                                    width={520}
+                                    height={400}
+                                    alt="Scary robot"
+                                />
+                                <VideoChatMessage />
+                                <VideoBar />
+                            </div>
+                        </div>
                     </div>
+                    <Gradient />
                 </div>
             </div>
         </Section>
