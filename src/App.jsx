686a9320f1cd3e0d68cf3145c006a8e63ddc3a46
Fathiya Olatokunbo Jamia <fathjami@Achrafs-MacBook-Pro.local>
2024-05-23 12:26:35 +0100

    roadmap done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Benefits from './components/Benefits'
 import Collaboration from './components/Collaboration'
 import Services from './components/Services'
 import Pricing from './components/Pricing'
+import Roadmap from './components/Roadmap'
 
 const App = () => {
     return (
@@ -17,6 +18,7 @@ const App = () => {
                 <Collaboration />
                 <Services />
                 <Pricing />
+                <Roadmap />
             </div>
         </>
     )
